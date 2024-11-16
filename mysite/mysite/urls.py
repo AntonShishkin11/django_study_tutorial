a6c8db9c7d88fa48c7c09aeb3c5458073b4379df
Anton <annnt9na@yandex.ru>
2024-11-16 23:30:21 +0300

    Created tests & homepage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from polls.views import homepage
+
 urlpatterns = [
+path('', homepage, name='home'),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 
